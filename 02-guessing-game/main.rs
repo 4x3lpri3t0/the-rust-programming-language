2af3bc710e136ffe45dfeb46896f95d132c75ae2
Axel Prieto <prietoaxel@gmail.com>
2020-11-28 16:33:25 +0000

    minor changes

---
+++ 02-guessing-game/main.rs
@@ -0,0 +1,21 @@
+fn main_1() {
+    let mut guess = String::new();
+    println!("Hellrustup default stable o world!");
+}
+
+// Using a ! means that you’re calling a macro instead of a normal function.
+// Compile: rustc main.rs
+
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stin().read_line(&mut guess)
+        .expect("Failed to read");
+    
+    println!("You guessed: {}", guess);
+}
--- main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut guess = String::new();
-    println!("Hellrustup default stable o world!");
-}
-
-// Using a ! means that you’re calling a macro instead of a normal function.
-// Compile: rustc main.rs

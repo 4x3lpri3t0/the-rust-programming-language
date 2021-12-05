417721232bc9f15bd95871193cd795a71dc29ca6
Axel Prieto <prietoaxel@gmail.com>
2021-12-05 00:39:13 +0000

    minor changes

--- 06-enums/src/main.rs
+++ 06-enums/src/main.rs
@@ -1,5 +1,51 @@
+// Enums and Pattern Matching
+
 fn main() {
-    // Enums and Pattern Matching
+    // Defining an Enum
+
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    // Create instances like this:
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    // We can define a fn that takes any IpAddrKind:
+
+    fn route(ip_kind: IpAddrKind) {}
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // To also store the actual IP address _data_, we can use a struct:
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    // We can represent the same concept using just an enum. This new definition of the IpAddr enum says that both V4 and V6 variants will have associated String values:
+
+    enum IpAddr2 {
+        V4(String),
+        V6(String),
+    }
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
 
     // TODO ...
 }

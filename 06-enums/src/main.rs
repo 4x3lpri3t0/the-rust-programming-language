bf03ed2a6d70bd5c752398f470ae3422c3865c74
Axel Prieto <prietoaxel@gmail.com>
2021-12-12 12:47:25 +0000

    minor changes

--- 06-enums/src/main.rs
+++ 06-enums/src/main.rs
@@ -47,5 +47,40 @@ fn main() {
     let home = IpAddr2::V4(String::from("127.0.0.1"));
     let loopback = IpAddr2::V6(String::from("::1"));
 
+    // If we wanted to store V4 addresses as four u8 values but still express V6 addresses as one String value, we wouldn’t be able to with a struct. Enums handle this case with ease:
+
+    enum IpAddr3 {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr3::V6(String::from("::1"));
+
+    // Another example of an enum with a wide variaty of types embedded in its variants:
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    // Just as we’re able to define methods on structs using impl, we’re also able to define methods on enums. Here’s a method named call that we could define on our Message enum:
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    // The body of the method would use self to get the value that we called the method on. In this example, we’ve created a variable m that has the value Message::Write(String::from("hello")), and that is what self will be in the body of the call method when m.call() runs.
+
+    // The Option Enum and Its Advantages Over Null Values
+
     // TODO ...
 }

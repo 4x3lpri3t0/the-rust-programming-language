ba4b68877e1f2e19b43f0f8fed0da0665dd33c54
Axel Prieto <prietoaxel@gmail.com>
2022-03-15 00:07:19 +0000

    Daily programming practice

--- 04-ownership/src/main.rs
+++ 04-ownership/src/main.rs
@@ -26,11 +26,59 @@ fn main() {
 
     // Memory and Allocation:
 
-    let s = String::from("hello"); // Actual String, not literal
-                                   // When a variable goes out of scope, Rust calls the 'drop' function for us.
-                                   // (e.g.: Rust calls drop automatically at the closing curly bracket)
+    let _s = String::from("hello"); // Actual String, not literal
+                                    // When a variable goes out of scope, Rust calls the 'drop' function for us.
+                                    // (e.g.: Rust calls drop automatically at the closing curly bracket)
+
+    // --
 
     // Ways Variables and Data Interact: Move
+
+    // Try to use s1 after s2 is created:
+    let s1 = String::from("hello");
+    let _s2 = s1;
+    // println!("{}, world!", s1);
+    // -> Error! ^^ value used here after move
+    // ... It invalidates the first variable (Move).
+    // Instead of trying to copy the allocated memory, Rust considers s1 to no longer be valid.
+    // With only s2 valid, when it goes out of scope, it alone will free the memory.
+
+    // [!!!] Rust will never automatically create "deep" copies of your data.
+
+    //--
+
+    // Ways Variables and Data Interact: Clone
+
+    // Clone: Common method to deeply copy the heap data of the String (not just the stack data).
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    // --
+
+    // Stack-Only Data: Copy
+
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+    // This works though, because types such as integers that have a known size at compile time
+    // are stored entirely on the stack, so copies of the actual values are quick to make.
+
+    // Any group of simple scalar values can be Copy.
+    // Nothing that requires allocation or is some form of resource is copy.
+
+    // Some of the types that are Copy:
+    // * Integer types (u32)
+    // * bool
+    // * chat
+    // * Floating point types (f64)
+    // * Tuples (if they only contain types that are also Copy)
+
+    // --
+
+    // OWNERSHIP and FUNCTIONS
 
     // TODO ...
 }

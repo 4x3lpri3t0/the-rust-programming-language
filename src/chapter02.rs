4a60e03d39bab41befc1318bd91bc715a865dacb
Axel Prieto <prietoaxel@gmail.com>
2022-03-30 13:16:09 +0200

    Refactor chapter 2 - guessing game

---
+++ src/chapter02.rs
@@ -0,0 +1,48 @@
+pub mod c02 {
+
+    use rand::Rng;
+    use std::cmp::Ordering;
+    use std::io;
+
+    pub fn guessing_game() {
+        println!("*** Chapter 2 - Guess the Number! ***");
+
+        let secret_number = rand::thread_rng().gen_range(1..101);
+        let mut attempts = 1;
+
+        loop {
+            // Using a ! means that you’re calling a macro instead of a normal function.
+            println!("Please input your guess.");
+
+            // In Rust, variables are immutable by default
+            let mut guess = String::new(); // String is UTF-8 encoded
+
+            io::stdin()
+                .read_line(&mut guess) // Returns io::Result
+                .expect("Failed to read line");
+
+            // Can "shadow" the previous value of guess with a new one.
+            // Used in situations in which you want to convert a value from one type to another type.
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+                // Switching from an expect call to a match expression is how you generally
+                // move from crashing on an error to handling the error.
+            };
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You guessed the number after {} attempts!", attempts);
+                    break;
+                }
+            }
+
+            println!("You guessed: {}", guess);
+            attempts += 1;
+        }
+
+        println!("The secret number is: {}", secret_number);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 // ^ So we can write examples without the compiler complaining about warnings.
+mod chapter02;
+
 mod chapter05;
 mod chapter06;
 mod chapter07;
+
+pub use crate::chapter02::c02;
 
 pub use crate::chapter05::c05;
 pub use crate::chapter06::c06;
 pub use crate::chapter07::c07;
 
 fn main() {
+    c02::guessing_game();
     // c05::structs();
     // c06::enums();
-    c07::packages_crates_modules();
+
+    // c07::packages_crates_modules();
 }
